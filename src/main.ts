33c4d3abfef18216704612787f2c8c903240620c
maxum2003la <maxum2003la@gmail.com>
2025-05-14 02:02:19 +0300

    fix: add domen for cors

--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,16 @@ import { ResponseInterceptor } from './shared/response/response.interceptors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ResponseInterceptor());
-  // app.enableCors();
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://learnlog-fe.netlify.app',
+    ],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
-
+  
   const configService = app.get<ConfigService>(ConfigService);
   const port = configService.get<number>('app.port');
 
